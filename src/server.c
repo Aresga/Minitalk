80e47132900befa5755810e3b505b8dabb411251
Abderrahman Gaga <gagabdel@gmail.com>
2025-02-18 13:57:21 +0200

    modified:   src/server.c
    	modified:   src/server.c

--- src/server.c
+++ src/server.c
@@ -6,7 +6,11 @@
 /*   By: agaga <agaga@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:47:55 by agaga             #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/02/07 17:19:38 by agaga            ###   ########.fr       */
+=======
+/*   Updated: 2025/02/18 13:54:58 by agaga            ###   ########.fr       */
+>>>>>>> b779aa6 (	modified:   src/server.c)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +20,8 @@
 
 void	sig_handler(int sig)
 {
-	static int				index;
-	static unsigned char	c;
+	static int				index = 0;
+	static unsigned char	c = 0;
 
 	if (sig == SIGUSR1)
 		c = c | (1 << (7 - index));
@@ -30,14 +34,44 @@ void	sig_handler(int sig)
 	}
 }
 
+// int	main(void)
+// {
+// 	ft_printf("Server PID: %d\n", getpid());
+// 	signal(SIGUSR1, sig_handler);
+// 	signal(SIGUSR2, sig_handler);
+// 	while (1)
+// 	{
+// 		pause();
+// 	}
+// 	return (0);
+// }
+
 int	main(void)
 {
-	ft_printf("Server PID: %d\n", getpid());
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
-	while (1)
-	{
-		pause();
-	}
-	return (0);
+    struct sigaction	sa;
+
+    ft_printf("Server PID: %d\n", getpid());
+    
+    // Initialize sigaction struct
+    sa.sa_handler = sig_handler;
+    sa.sa_flags = SA_RESTART;  // Automatically restart interrupted system calls
+    sigemptyset(&sa.sa_mask);  // Initialize empty signal mask
+    
+    // Set up signal handlers
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        ft_printf("Error setting up SIGUSR1 handler\n");
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        ft_printf("Error setting up SIGUSR2 handler\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (1)
+    {
+        pause();
+    }
+    return (0);
 }
