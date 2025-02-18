12bd5115e902c632b275dd1ffbab4fce4364bba7
Abderrahman Gaga <gagabdel@gmail.com>
2025-02-18 14:38:37 +0200

    testing message confirm

--- src/client.c
+++ src/client.c
@@ -6,70 +6,142 @@
 /*   By: agaga <agaga@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:44:13 by agaga             #+#    #+#             */
-/*   Updated: 2025/02/07 17:18:43 by agaga            ###   ########.fr       */
+/*   Updated: 2025/02/18 14:34:33 by agaga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include "../libft/inc/libft.h"
+// #include <signal.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+
+// // Thi function sends a signal to the server
+// // The signal is either SIGUSR1 or SIGUSR2
+// // SIGUSR1 is sent if the bit is 1
+// // SIGUSR2 is sent if the bit is 0
+// void	seg_send(int pid, unsigned char c)
+// {
+// 	int	index;
+
+// 	index = 8;
+// 	while (index-- > 0)
+// 	{
+// 		if ((c >> index) & 1)
+// 			kill(pid, SIGUSR1);
+// 		else 
+// 			kill(pid, SIGUSR2);
+// 		usleep(800);
+// 	}
+// }
+
+// int	check_pid(char *pid)
+// {
+// 	int	index;
+
+// 	index = 0;
+// 	while (pid[index])
+// 	{
+// 		if (!ft_isdigit(pid[index]))
+// 			return (0);
+// 		index++;
+// 	}
+// 	return (1);
+// }
+
+// int	main(int ac, char **av)
+// {
+// 	int			server_pid;
+// 	int			index;
+
+// 	if (ac != 3)
+// 	{
+// 		ft_printf("Usage: %s <PID> <MESSAGE>\n", av[0]);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	if (!check_pid(av[1]))
+// 	{
+// 		ft_printf("Invalid PID\n");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	server_pid = ft_atoi(av[1]);
+// 	index = 0;
+// 	while (av[2][index])
+// 	{
+// 		seg_send(server_pid, av[2][index]);
+// 		index++;
+// 	}
+// 	seg_send(server_pid, '\n');
+// 	return (0);
+// }
+
 
 #include "../libft/inc/libft.h"
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// Thi function sends a signal to the server
-// The signal is either SIGUSR1 or SIGUSR2
-// SIGUSR1 is sent if the bit is 1
-// SIGUSR2 is sent if the bit is 0
-void	seg_send(int pid, unsigned char c)
-{
-	int	index;
+static volatile sig_atomic_t g_received = 0;
 
-	index = 8;
-	while (index-- > 0)
-	{
-		if ((c >> index) & 1)
-			kill(pid, SIGUSR1);
-		else 
-			kill(pid, SIGUSR2);
-		usleep(800);
-	}
+void    confirm_handler(int sig)
+{
+    if (sig == SIGUSR1)
+        g_received = 1;
 }
 
-int	check_pid(char *pid)
+void    seg_send(int pid, unsigned char c)
 {
-	int	index;
+    int    index;
 
-	index = 0;
-	while (pid[index])
-	{
-		if (!ft_isdigit(pid[index]))
-			return (0);
-		index++;
-	}
-	return (1);
+    index = 8;
+    while (index-- > 0)
+    {
+        g_received = 0;
+        if ((c >> index) & 1)
+            kill(pid, SIGUSR1);
+        else 
+            kill(pid, SIGUSR2);
+        
+        // Wait for confirmation
+        while (!g_received)
+            usleep(100);
+    }
 }
 
-int	main(int ac, char **av)
+int    main(int ac, char **av)
 {
-	int			server_pid;
-	int			index;
+    struct sigaction    sa;
+    int                server_pid;
+    int                index;
 
-	if (ac != 3)
-	{
-		ft_printf("Usage: %s <PID> <MESSAGE>\n", av[0]);
-		exit(EXIT_FAILURE);
-	}
-	if (!check_pid(av[1]))
-	{
-		ft_printf("Invalid PID\n");
-		exit(EXIT_FAILURE);
-	}
-	server_pid = ft_atoi(av[1]);
-	index = 0;
-	while (av[2][index])
-	{
-		seg_send(server_pid, av[2][index]);
-		index++;
-	}
-	seg_send(server_pid, '\n');
-	return (0);
+    if (ac != 3)
+    {
+        ft_printf("Usage: %s <PID> <MESSAGE>\n", av[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up signal handler for confirmation
+    sa.sa_handler = confirm_handler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        ft_printf("Error setting up signal handler\n");
+        exit(EXIT_FAILURE);
+    }
+
+    server_pid = ft_atoi(av[1]);
+    if (server_pid <= 0)
+    {
+        ft_printf("Invalid PID\n");
+        exit(EXIT_FAILURE);
+    }
+
+    index = 0;
+    while (av[2][index])
+    {
+        seg_send(server_pid, av[2][index]);
+        index++;
+    }
+    seg_send(server_pid, '\0');
+    return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,11 +6,7 @@
 /*   By: agaga <agaga@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:47:55 by agaga             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/02/07 17:19:38 by agaga            ###   ########.fr       */
-=======
-/*   Updated: 2025/02/18 13:54:58 by agaga            ###   ########.fr       */
->>>>>>> b779aa6 (	modified:   src/server.c)
+/*   Updated: 2025/02/18 14:34:01 by agaga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +14,48 @@
 #include <signal.h>
 #include <unistd.h>
 
-void	sig_handler(int sig)
+void    sig_handler(int sig, siginfo_t *info, void *context)
 {
-	static int				index = 0;
-	static unsigned char	c = 0;
+    static int              index = 0;
+    static unsigned char    c = 0;
+    pid_t                   client_pid;
 
-	if (sig == SIGUSR1)
-		c = c | (1 << (7 - index));
-	index++;
-	if (index == 8)
-	{
-		write (1, &c, 1);
-		index = 0;
-		c = 0;
-	}
+    (void)context;
+    client_pid = info->si_pid;
+
+    if (sig == SIGUSR1)
+        c |= (1 << (7 - index));
+    index++;
+
+    // Send confirmation signal
+    kill(client_pid, SIGUSR1);
+
+    if (index == 8)
+    {
+        write(1, &c, 1);
+        index = 0;
+        c = 0;
+    }
 }
+
+// void	sig_handler(int sig)
+// {
+// 	static int				index = 0;
+// 	static unsigned char	c = 0;
+
+// 	if (sig == SIGUSR1)
+// 		c = c | (1 << (7 - index));
+// 	index++;
+// 	if (index == 8)
+// 	{
+// 		if (c == '\0')
+// 			write (1, "\n", 1);
+// 		else
+// 			write (1, &c, 1);
+// 		index = 0;
+// 		c = 0;
+// 	}
+// }
 
 // int	main(void)
 // {
@@ -46,32 +69,24 @@ void	sig_handler(int sig)
 // 	return (0);
 // }
 
-int	main(void)
+int    main(void)
 {
-    struct sigaction	sa;
+    struct sigaction    sa;
 
     ft_printf("Server PID: %d\n", getpid());
     
-    // Initialize sigaction struct
-    sa.sa_handler = sig_handler;
-    sa.sa_flags = SA_RESTART;  // Automatically restart interrupted system calls
-    sigemptyset(&sa.sa_mask);  // Initialize empty signal mask
+    sa.sa_sigaction = sig_handler;
+    sa.sa_flags = SA_SIGINFO | SA_RESTART;
+    sigemptyset(&sa.sa_mask);
     
-    // Set up signal handlers
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
+        sigaction(SIGUSR2, &sa, NULL) == -1)
     {
-        ft_printf("Error setting up SIGUSR1 handler\n");
-        exit(EXIT_FAILURE);
-    }
-    if (sigaction(SIGUSR2, &sa, NULL) == -1)
-    {
-        ft_printf("Error setting up SIGUSR2 handler\n");
+        ft_printf("Error setting up signal handlers\n");
         exit(EXIT_FAILURE);
     }
     
     while (1)
-    {
         pause();
-    }
     return (0);
 }
